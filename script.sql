0ce1268134c8f2b9d39a5ba10101bc4d939609d4
axel emmanuel angeles rodriguez <2173072727@correo.ler.uam.mx>
2023-06-22 17:46:53 -0600

    agregamos reputacion a proveedor agregamos la clave foranea id_servicios a medicacion afinamos temas de cantidad de consumibles

--- script.sql
+++ script.sql
@@ -40,6 +40,7 @@ create table proveedor (
     telefono varchar(10) not null,
     email varchar(50) not null,
     id_direccion int not null,
+    reputacion varchar(50) not null,
     foreign key (id_direccion) references direccion(id_direccion)
 );
 create table medicamento (
@@ -54,18 +55,21 @@ create table medicamento (
 );
 create table medicacion (
     id_medicacion serial primary key,
+    id_servicio int not null,
     id_medicamento int not null,
     id_facultativo int not null,
     fecha_de_entrega date not null,
     cantidad int not null,
     foreign key (id_medicamento) references medicamento(id_medicamento),
     foreign key (id_facultativo) references facultativos(id_facultativo)
+    foreign key (id_servicio) references servicio(id_servicio)
 );
 create table consumibles (
     id_consumible serial primary key,
     nombre_consumible varchar(50) not null,
     precio float not null,
-    cantidad int not null,
+    cantidad_consumida int not null,
+    cantidad_por_envase int not null,
     id_proveedor int not null,
     foreign key (id_proveedor) references proveedor(id_proveedor)
 );
